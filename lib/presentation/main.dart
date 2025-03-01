7c67d47119b05ac9d0fcdbb29b9192489093d121
Rahul <rahul.gamedev@gmail.com>
2025-03-02 00:08:24 +0530

    Change theme mode to system for better user experience

--- lib/presentation/main.dart
+++ lib/presentation/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       home: const Chat(),
     );
   }
