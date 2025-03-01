25f03afe2a876a6017ead671f7f71b7242c12c0d
Rahul <rahul.gamedev@gmail.com>
2025-03-02 00:18:19 +0530

    Update dark theme color for improved contrast

--- lib/domain/config/theme/app_color.dart
+++ lib/domain/config/theme/app_color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const light = Color(0xFFFAFAFA);
-  static const dark = Color(0xFF050506);
+  static const dark = Color(0xFF0E0F11);
   static const accent = Color(0xFFD80E0E);
   static const grey = Color(0xFF7E7E7E);
 }
